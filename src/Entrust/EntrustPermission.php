d508aef882e47177b095edc1c73c0e9a84a4b1b7
Arvindkumar Badwal <arvindkumar.badwal@ambab.com>
2018-09-24 11:38:17 +0530

    first commit

---
+++ src/Entrust/EntrustPermission.php
@@ -0,0 +1,38 @@
+<?php namespace Arvindkumar\EntrustMongoDB;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Arvindkumar\EntrustMongoDB
+ */
+
+use Arvindkumar\EntrustMongoDB\Contracts\EntrustPermissionInterface;
+use Arvindkumar\EntrustMongoDB\Traits\EntrustPermissionTrait;
+use Jenssegers\Mongodb\Eloquent\Model as Model;
+use Illuminate\Support\Facades\Config;
+
+class EntrustPermission extends Model implements EntrustPermissionInterface
+{
+    use EntrustPermissionTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = Config::get('entrust.permissions_table');
+    }
+
+}
